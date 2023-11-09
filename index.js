f7c96477e0afa427a18894c2bee6406a9274b3a5
overlord70 <s37776270@gmail.com>
2023-11-09 16:31:42 +0500

    dont know

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-let attempt = 3
-let pwd
-while(pwd !== 777){
-   pwd = +prompt('What is your password?')
-   if(pwd === 777){
-        alert('Welcome')
-   } else{
-       alert('Your password is wrong' + ` You have ${attempt} attempts`) 
-   }
-   attempt--;
-   if(attempt === -1){
-       while(attempt === -1){
-           alert('Try later')
-            }
-       }
-   }
+ let attempt = 3
+ let pwd
+ while(pwd !== 777){
+    pwd = +prompt('What is your password?')
+    if(pwd === 777){
+         alert('Welcome')
+    } else{
+        alert('Your password is wrong' + ` You have ${attempt} attempts`) 
+    }
+    attempt--;
+    if(attempt === -1){
+        while(attempt === -1){
+            alert('Try later')
+             }
+        }
+    }
 
-// let times = 3
-// let number = prompt('What number do you want to choose?')
-//     for(let i = 1; i <= 10; i++){
-//         console.log(`${number} * ${i} =${number * i}`)
+//  let times = 3
+//  let number = prompt('What number do you want to choose?')
+//      for(let i = 1; i <= 10; i++){
+//          alert(`${number} * ${i} =${number * i}`)
 //     }
-//
